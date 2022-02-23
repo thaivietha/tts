a5923740f135c036fb0203379efbb90dc1218558
Your Name <123thth2014@gmail.com>
2022-02-23 21:02:46 +0700

    first

---
+++ config/locale.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'en' => [
+        'code' => 'EN',
+        'display' => 'English',
+        'flag' => 'us'
+    ],
+    'vi' => [
+        'code' => 'VI',
+        'display' => 'Tiếng Việt',
+        'flag' => 'vi'
+    ],  
+    'es' => [
+        'code' => 'ES',
+        'display' => 'Español',
+        'flag' => 'es'
+    ], 
+	'fr' => [
+        'code' => 'FR',
+        'display' => 'Français',
+        'flag' => 'fr'
+    ], 	
+];
